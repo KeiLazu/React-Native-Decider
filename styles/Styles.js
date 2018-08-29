8f153dccd6b8b94898f2c852d2a5dd12e1739c66
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-08-29 14:32:00 +0700

    add a bit of structure

---
+++ styles/Styles.js
@@ -0,0 +1,12 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const Style = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
+
+export default Styles;
